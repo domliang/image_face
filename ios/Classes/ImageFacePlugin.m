ece593cf775e93ecf5e93668eedfb8c68865f300
DOMLIANG <pandaqiang@gmail.com>
2020-06-10 19:32:22 +0800

    feat: 添加ios人脸识别
    基于coreimage

--- ios/Classes/ImageFacePlugin.m
+++ ios/Classes/ImageFacePlugin.m
@@ -11,27 +11,34 @@
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
   if ([@"hasFace" isEqualToString:call.method]) {
-      
-    result(@(NO));
+      NSString *p = [call.arguments objectForKey:@"image"];
+      BOOL rst =[self checkFace:  p];
+      result(@(rst));
   } else {
     result(FlutterMethodNotImplemented);
   }
 }
 
-//-(Boolean)checkFace:(NSString*)p{
-//    
-////    CIImage *myImage = [CIImage imageWithContentsOfURL:(nonnull NSURL *)]
-////
-////    CIContext *context = [CIContext context];                    // 1
-////    NSDictionary *opts = @{ CIDetectorAccuracy : CIDetectorAccuracyHigh };      // 2
-////    CIDetector *detector = [CIDetector detectorOfType:CIDetectorTypeFace
-////                                              context:context
-////                                              options:opts];                    // 3
-////
-////    opts = @{ CIDetectorImageOrientation :
-////              [[myImage properties] valueForKey:kCGImagePropertyOrientation] }; // 4
-////    NSArray *features = [detector featuresInImage:myImage options:opts];        // 5
-//    
-//}
+- (BOOL)checkFace:(NSString*)p{
+    
+    NSURL * urlStr = [NSURL fileURLWithPath:p];
+
+    CIImage *myImage = [CIImage imageWithContentsOfURL:urlStr];
+
+    CIContext *context = [CIContext context];                    // 1
+    NSDictionary *opts = @{ CIDetectorAccuracy : CIDetectorAccuracyHigh };      // 2
+    CIDetector *detector = [CIDetector detectorOfType:CIDetectorTypeFace
+                                              context:context
+                                              options:opts];                    // 3
+
+    opts = @{ CIDetectorImageOrientation :
+              [[myImage properties] valueForKey:kCGImagePropertyOrientation] }; // 4
+    NSArray *features = [detector featuresInImage:myImage options:opts];        // 5
+    if (features.count >0) {
+        return YES;
+    } else {
+        return NO;
+    }
+}
 
 @end
